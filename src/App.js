4b6443c338a5d9c168dbef4533ec014ac3ff73cd
cdsalty <soltiscd@gmail.com>
2019-05-05 22:38:35 -0400

    added onClick button sep file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import RandoNumber from './RandoNumber';
+// import Button from './Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <RandoNumber maxNumber={7} />
+        <Button />
       </div>
     );
   }
